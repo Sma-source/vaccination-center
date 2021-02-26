8c61ced7def34530edcc15eff74dfab4d8b7ebbf
Sma-source <rabhi.smain@gmail.com>
2021-02-26 23:17:44 +0100

    update Center component to render card info

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-const Center = ({ clinic, ville }) => {
+const Center = ({ center }) => {
   return (
-    <div className="cocktail-footer">
-      <h3>{ville}</h3>
-      <h4>{clinic}</h4>
+    <div className="col-md-4 col-xs-12">
+      <div className="legacy-panel">
+        <span className="icon"></span>
+        <div className="project-card-header">
+          <h4>{center.properties.c_com_nom}</h4>
+        </div>
+        <div className="project-card-description">
+          <p>{center.properties.c_nom}</p>
+        </div>
+      </div>
     </div>
   );
 };
