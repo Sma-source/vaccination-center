2cb899f45862b94e0b0bdb510575c4b684a299ce
Sma-source <rabhi.smain@gmail.com>
2021-02-27 00:09:13 +0100

    add icon vaccine

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import vaccineIcon from "@iconify-icons/tabler/vaccine";
 
 const Center = ({ center }) => {
   return (
     <div className="col-md-4 col-xs-12">
       <div className="legacy-panel">
-        <span className="icon"></span>
+        <span className="icon">
+          <Icon className="icon" icon={vaccineIcon} />
+        </span>
         <div className="project-card-header">
           <h4>{center.properties.c_com_nom}</h4>
         </div>
